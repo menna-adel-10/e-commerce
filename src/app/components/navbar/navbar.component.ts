8bffa2f6251dd921f38e954824abd14b93a79714
menna-adel-10 <menna10_adel@hotmail.com>
2024-02-19 16:24:32 +0200

    modified:   src/app/components/footer/footer.component.html 	modified:   src/app/components/navbar/navbar.component.html 	modified:   src/app/components/navbar/navbar.component.ts 	modified:   src/app/components/search-nav/search-nav.component.ts

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './navbar.component.html',
 })
 export class NavbarComponent {
 
+  isNavbarFixed: boolean = false;
+
+  @HostListener('window:scroll', ['$event']) onScroll() {
+    if (window.scrollY > 1) {
+      this.isNavbarFixed = true;
+    } else {
+      this.isNavbarFixed = false;
+    }
+  }
 }
--- src/app/components/search-nav/search-nav.component.ts
+++ src/app/components/search-nav/search-nav.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-search-nav',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-nav.component.html',
 })
 export class SearchNavComponent {
 
+    isNavBarHidden: boolean = false;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    // Here you can define the logic to hide the navigation bar when scrolling
+    this.isNavBarHidden = window.scrollY > 0; // You can adjust the threshold value as per your requirement
+  }
 }
